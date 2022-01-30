4f47f1a01e84131ea24b7bd3a767446156deaff0
maxi barbo <maxi10lb@gmail.com>
2022-01-29 23:05:33 -0300

    calculos.js

---
+++ js/calculos.js
@@ -0,0 +1,56 @@
+$(function(){
+
+//Simulador Cambio Usd/Ars y eur/Ars
+
+let dolar = 200;
+let euro = 250;
+let btc = 60000;
+let eth = 4000;
+let bake = 2.5;
+
+
+    $("#usdArs").click(function convertir(pesos) {
+    
+        pesos = $("[name*='dolar']").val();
+
+        let resultadoDolar = multi(pesos,dolar);
+        let resultadoIvaDolar = sumarIva(resultadoDolar);
+
+        alert(`Ingresaste: ${pesos} Ars
+        Sin Iva: ${resultadoDolar} Usd
+        Con iva: ${resultadoIvaDolar} Usd`);
+
+    $("#eurArs").click(function convertir(pesos){
+
+        pesos = $("[name*='dolar']").val();
+
+        let resultadoEuro = multi(pesos,euro);
+        let resultadoIva = sumarIva(resultadoEuro);
+        alert(`Ingresaste: ${pesos} Ars
+        Sin Iva: ${resultadoEuro} Euros
+        Con iva: ${resultadoIva} Euros`);
+    });  
+    
+    $("#btcArs").click(function convertir(pesos){
+
+        pesos = $("[name*='dolar']").val();
+        let resultadobtc = multi(pesos,btc);
+        
+        alert(`Ingresaste: ${pesos} Ars
+        Son: ${resultadobtc} BTC`);
+    }); 
+
+
+
+   
+});
+
+function multi(a,b){
+    return a/b;
+} 
+
+function sumarIva(precioCosto){
+    return (precioCosto + (precioCosto * 0.21));
+    }
+
+});
--- js/simuladores.js
+++ js/simuladores.js
@@ -58,69 +58,6 @@ function valorDolar(moneda,resultado){
     return a*b;
     } 
 
-//Funcion para calcular cambio divisa relacion ars/Usd - ars/euro
-
-function valorDolar(moneda,resultado){
-
-    moneda = prompt("Ingrese dolar o euro").toLowerCase();
-
-    resultado = parseInt(prompt("Ingrese Cantidad Deseada"));
-
-    let resultadoDolar = multi(resultado,dolar);
-
-    let resultadoEuro = multi(resultado,euro);
-
-    pregunta = confirm("iva incluido?")
-
-    iva1 = sumarIva(resultadoEuro);
-    iva2 = sumarIva(resultadoDolar);
-
-    if  (isNaN(resultado)){
-    alert ("No ingresaste un numero")
-          
-}
-    if (moneda !=="dolar" && moneda !=="euro"){
-        alert ("No ingresaste ni euro ni dolar")
-
-    }
-    else if (moneda === "dolar"){
-       
-        if(pregunta){
-        alert("Con Iva $" + iva2 + " Ars")
-            
-        }
-        else if (pregunta === false);{
-        alert("Sin Iva $" + resultadoDolar + " Ars");
-            
-        }     
-    }
-    
-    else if(moneda === "euro"){
-
-        if(pregunta === "si"){
-            alert("Con Iva $" + iva1 + " Ars")
-                
-        }
-        else if (pregunta === "no");{
-        alert("Sin Iva $" + resultadoEuro + " Ars");
-            
-        } 
-}
-    else (false);{
-    alert("Desea cotizar nuevamente?")
-    }
-}   
-    var dolar = 200;
-    var euro = 250;
-    
-    function sumarIva(precioCosto){
-    return (precioCosto + (precioCosto * 0.21));
-    }
-
-    function multi(a,b){
-    return a*b;
-    } 
-
 // Funcion para Calcular cuantos USDT necesito para comprar las siguientes criptos BTC - ETH - BAKE
 // usdt_btc = "Numero Ingresado" * Valor BTC
 // usdt_eth = "numero ingresado" * valor ETH
@@ -129,7 +66,7 @@ function valorDolar(moneda,resultado){
 
 function calcripto(cripto,usdt){
 
-    cripto = prompt("Ingrese Cripto BTC - ETH - BAKE").toLocaleLowerCase();
+    cripto = prompt("Ingrese Cripto BTC - ETH - BAKE").toLowerCase();
     usdt = parseInt(prompt("Ingrese USDT"));
 
     let resultadoBtc = diferencia(usdt,btc);
